f43a2e0711adf598026b9e6c10ffb59c82b120cf
Oscar Mendoza <oscarmendozach@gmail.com>
2021-04-05 12:52:47 -0400

    EDA in Jupyter Notebook using matplotlib. The same analysis is been done in RStudio using ggplot2

--- CaseStudy1.R
+++ CaseStudy1.R
@@ -35,3 +35,51 @@ whitewine$color <- "white"
 
 df_wine <- rbind(redwine, whitewine)
 
+#EXPLORATORY DATA ANALYSIS
+
+#fixex acidity histogram
+ggplot(data = df_wine, aes(x = fixed.acidity)) + geom_histogram() 
+
+#total sulfur dioxide histogram
+ggplot(data = df_wine, aes(x = total.sulfur.dioxide)) + geom_histogram()
+
+#alcohol histogram
+ggplot(data = df_wine, aes(x = alcohol)) + geom_histogram()
+
+#pH histogram
+ggplot(data = df_wine, aes(x= pH)) + geom_histogram()
+
+#quality vs volatile acidity
+ggplot(data = df_wine, aes(x = quality, y = volatile.acidity)) + geom_point()
+
+#quality vs residual sugar
+ggplot(data = df_wine, aes(x = quality, y = residual.sugar)) + geom_point()
+
+#quality vs pH
+ggplot(data = df_wine, aes(x = quality, y = pH)) + geom_point()
+
+#quality vs alcohol
+ggplot(data = df_wine, aes(x = quality, y = alcohol)) + geom_point()
+
+#GROUPING
+#means by quality
+df_wine %>% group_by(quality) %>% summarise_all(.funs = c(mean = "mean"))
+
+#means by color and quality
+df_wine %>% group_by(color, quality) %>% summarise_all(.funs = c(mean = "mean"))
+
+#means by color and quality only using pH
+df_wine %>% select(color, quality, pH) %>%
+  group_by(quality, color) %>% 
+  summarise_all(.funs = c(pH_mean = "mean"))
+
+#means by color
+df_wine %>% group_by(color) %>% summarise_all(.funs = c(mean = "mean"))
+
+#what level of acidity receives the highest average rating?
+summary(df_wine['pH'])
+
+df3 <- df_wine %>% mutate(acidity.levels = case_when(pH < 3.11 ~ 'High',
+                                              pH >= 3.11 & pH < 3.21 ~ 'Mod_high',
+                                              pH >= 3.21 & pH < 3.32 ~ 'Medium',
+                                              pH >= 3.32 & pH <=4.010 ~ 'Low'))
